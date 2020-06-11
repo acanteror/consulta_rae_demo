6fde59b13309461f2da292e080fdf475e2625f28
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-11 20:27:29 +0200

    remove blocprovider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,48 +12,43 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Form(
-                key: _formKey,
-                child: TextFormField(
-                  decoration:
-                      InputDecoration(contentPadding: EdgeInsets.all(8)),
-                  keyboardType: TextInputType.text,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    _word = value;
-                  },
-                )),
-            Container(
-                padding: EdgeInsets.all(24),
-                child: Icon(
-                  Icons.error,
-                  color: Colors.red,
-                  size: 36,
-                )),
-          ],
-        )),
-      
-      floatingActionButton: BlocProvider(
-        create: (BuildContext context) => RaeBloc(),
-        child: FloatingActionButton(
-          onPressed: () async {
-            if (_formKey.currentState.validate()) {
-              print(_word);
-              context.bloc<RaeBloc>().add(RaeSubmit(word: _word));
-            }
-          },
-          tooltip: 'Search',
-          child: Icon(Icons.search),
-        ),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Form(
+              key: _formKey,
+              child: TextFormField(
+                decoration: InputDecoration(contentPadding: EdgeInsets.all(8)),
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  _word = value;
+                },
+              )),
+          Container(
+              padding: EdgeInsets.all(24),
+              child: Icon(
+                Icons.error,
+                color: Colors.red,
+                size: 36,
+              )),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            print(_word);
+            context.bloc<RaeBloc>().add(RaeSubmit(word: _word));
+          }
+        },
+        tooltip: 'Search',
+        child: Icon(Icons.search),
       ),
     );
   }
