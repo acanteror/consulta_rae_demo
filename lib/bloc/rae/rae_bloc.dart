b9a6000493a26b4d49047166fa9d3878679237a7
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-11 21:10:49 +0200

    update bloc

--- lib/bloc/rae/rae_bloc.dart
+++ lib/bloc/rae/rae_bloc.dart
@@ -2,11 +2,18 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:lumberdash/lumberdash.dart';
+import 'package:rae_test/exception/custom_exception.dart';
+import 'package:rae_test/service/rae_service.dart';
+import 'package:rae_test/utils/validate.dart';
 
 part 'rae_event.dart';
 part 'rae_state.dart';
 
 class RaeBloc extends Bloc<RaeEvent, RaeState> {
+  RaeService raeService;
+  RaeBloc({raeService}) : this.raeService = raeService ?? RaeService();
+
   @override
   RaeState get initialState => RaeInitial();
 
@@ -14,6 +21,23 @@ class RaeBloc extends Bloc<RaeEvent, RaeState> {
   Stream<RaeState> mapEventToState(
     RaeEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is RaeSubmit) {
+      yield* _submitToState(event);
+    }
+  }
+
+  Stream<RaeState> _submitToState(RaeSubmit event) async* {
+    final _word = event.word;
+    try {
+      final _response = await raeService.search(_word);
+      final _result = validateWord(_word, _response);
+      if (_result) {
+        yield RaeSuccess(result: _response);
+      } else {
+        yield RaeNotFound(word: _word);
+      }
+    } on ResponseException {
+      yield RaeError();
+    }
   }
 }
