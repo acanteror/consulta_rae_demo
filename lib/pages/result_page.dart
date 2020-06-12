6e87caef720219cf58eb75ed7b2f927c33aac3e6
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-12 19:24:42 +0200

    responsive

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rae_test/bloc/rae/rae_bloc.dart';
+import 'package:rae_test/extension/context_extension.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String _word = ''; 
+    String _word = '';
     String _result = '';
-    return BlocBuilder<RaeBloc, RaeState>(
-      builder: (context, state) {
-        if (state is RaeSuccess) {
-          _word = state.word;
-          _result = state.result;
-        }
-        return Scaffold(
-            appBar: AppBar(
-              title: Text(_word.toUpperCase()),
-            ),
-            body: SingleChildScrollView(
-                          child: Container(
-                  padding: EdgeInsets.all(18),
-                  alignment: Alignment.bottomLeft,
-                  child: Text(_result,
-                      style: TextStyle(
-                        fontSize: 18,
-                      ))),
-            ));
+    return BlocBuilder<RaeBloc, RaeState>(builder: (context, state) {
+      if (state is RaeSuccess) {
+        _word = state.word;
+        _result = state.result;
       }
-    );
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(_word.toUpperCase()),
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.all(context.pcw(5)),
+                alignment: Alignment.bottomLeft,
+                child: Text(_result,
+                    style: TextStyle(
+                      fontSize: context.pcw(4),
+                    ))),
+          ));
+    });
   }
 }
-
