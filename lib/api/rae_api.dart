4cb2e4e3d711abe91bc851008a331015342b2204
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-13 23:05:49 +0200

    add rae_api

---
+++ lib/api/rae_api.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'package:rae_test/exception/custom_exception.dart';
+
+abstract class RaeApi {
+  Future<String> fetchData(String word);
+}
+
+
+class RaeApiImpl extends RaeApi {
+  final http.Client client;
+
+  RaeApiImpl({client}): this.client = http.Client();
+
+
+  @override
+  Future<String> fetchData(String word) async {
+    final url = 'https://dle.rae.es/$word';
+    final response = await client.get(url);
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw ResponseException(response.statusCode);
+    }
+  }
+
+}
