ca0996163834ceb91e72062df0d7a1ae983d2646
Antonio Cantero Ruiz <antonio@aspgems.com>
2020-06-13 23:06:02 +0200

    refactor rae_service

--- lib/service/rae_service.dart
+++ lib/service/rae_service.dart
@@ -1,8 +1,16 @@
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' show parse;
+import 'package:rae_test/api/rae_api.dart';
 import 'package:rae_test/exception/custom_exception.dart';
 
-class RaeService {
+abstract class RaeService {
+  Future<String> consult(String word);
+}
+class RaeServiceImpl extends RaeService {
+  final RaeApiImpl raeApi;
+
+  RaeServiceImpl({raeApi}): this.raeApi = RaeApiImpl() ;
+
   Future<String> search(String word) async {
     final url = 'https://dle.rae.es/$word';
 
@@ -14,4 +22,26 @@ class RaeService {
       throw ResponseException(response.statusCode);
     }
   }
+
+  @override
+  Future<String> consult(String word) async {
+    final _result = await raeApi.fetchData(word);
+    final _description = parse(_result).getElementById('resultados').text;
+    bool _isValid = _validateWord(word, _description);
+    if (_isValid) {
+      return _description;
+    } else {
+      throw WordNotFoundException(word);
+    } 
+    
+
+  
+  }
+
+    bool _validateWord(String word, String result) {
+    bool isValid = true;
+    final error = 'Aviso: La palabra $word no está en el Diccionario.';
+    if (result.startsWith(error)) isValid = false;
+    return isValid;
+  } 
 }
