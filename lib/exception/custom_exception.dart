b15292282fe07b0c8a80538267fbfb0ce5bd9eef
Antonio Cantero <acr.cuentas@gmail.com>
2021-04-12 23:54:10 +0200

    Remove unused code

--- lib/exception/custom_exception.dart
+++ lib/exception/custom_exception.dart
@@ -1,25 +1,9 @@
-import 'package:lumberdash/lumberdash.dart';
-
 class ResponseException implements Exception {
   final message;
   ResponseException([this.message]);
-
-  call() => logError(this.toString());
-
-  String toString() {
-    if (message == null) return "Exception";
-    return "ResponseException: $message";
-  }
 }
 
 class WordNotFoundException implements Exception {
   final word;
   WordNotFoundException([this.word]);
-
-  call() => logError(this.toString());
-
-  String toString() {
-    if (word == null) return "Exception";
-    return "WordNotFoundException: La RAE no recoge la palabra $word.";
-  }
 }
